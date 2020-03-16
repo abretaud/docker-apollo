d0514082a1d132e2c4395b7e22ae89480a9aa80a
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2020-03-16 17:42:31 +0100

    WAit for DB before starting tomcat

--- launch.sh
+++ launch.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
-echo "Starting tomcat with $CATALINA_HOME"
+echo "Waiting for DB"
+# Hacky, will only work with postgres.
+
+DB_HOST=$(echo $WEBAPOLLO_DB_URI | sed 's|jdbc:postgresql://\([^/]\+\)/[^/]\+|\1|')
+DB_NAME=$(echo $WEBAPOLLO_DB_URI | sed 's|jdbc:postgresql://[^/]\+/\([^/]\+\)|\1|')
+DB_PORT="5432"
+
+echo
+echo "=> Trying to connect to a database using:"
+echo "      Database Host:     $DB_HOST"
+echo "      Database Port:     $DB_PORT"
+echo "      Database Username: $WEBAPOLLO_DB_USERNAME"
+echo "      Database Password: $WEBAPOLLO_DB_PASSWORD"
+echo "      Database Name:     $DB_NAME"
+echo
+
+for ((i=0;i<20;i++))
+do
+    DB_CONNECTABLE=$(PGPASSWORD=$WEBAPOLLO_DB_PASSWORD psql -U "$WEBAPOLLO_DB_USERNAME" -h "$DB_HOST" -p "$DB_PORT" -l >/dev/null 2>&1; echo "$?")
+	if [[ $DB_CONNECTABLE -eq 0 ]]; then
+		break
+	fi
+    sleep 3
+done
+
+if ! [[ $DB_CONNECTABLE -eq 0 ]]; then
+	echo "Cannot connect to database"
+    exit "${DB_CONNECTABLE}"
+fi
+
+echo "DB ready, starting tomcat with $CATALINA_HOME"
 $CATALINA_HOME/bin/startup.sh
 
 if [[ ! -f "${CATALINA_HOME}/logs/catalina.out" ]]; then
